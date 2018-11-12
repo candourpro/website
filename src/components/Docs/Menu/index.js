64c3c656b009953eb38d790f934c8260381500a8
Domas <domas.bitvinskas@me.com>
2018-11-12 17:08:58 +0000

    Add GitHub to side menu

--- src/components/Docs/Menu/index.js
+++ src/components/Docs/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'candour'
+import { Container, Text } from 'candour'
 
 import docs from '../../../docs/index'
 import Items from './Items'
@@ -7,5 +7,15 @@ import Items from './Items'
 export default (props) => (
   <Container padding borderRightLight minWidth={12} minHeight='100vh'>
     <Items items={docs} {...props} />
+
+    <Text
+      component='a'
+      href='https://github.com/candourpro/candour'
+      target='_blank'
+      level={props.level}
+      fontWeight700
+    >
+      GitHub
+    </Text>
   </Container>
 )
